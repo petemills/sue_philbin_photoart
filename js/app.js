f9f4dafa59e48040fd2545ddb469ec32158c4379
Pete <pete@pete.com>
2014-09-07 14:59:33 +1200

    MINOR CHANGES

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('sue-philbin-photoart', []);
+
+app.controller("InfoController", function(){
+	this.currentInfo = "aboutSue";
+
+	this.selectInfo = function(setInfo) {
+		this.currentInfo = setInfo
+	};
+
+	this.isSelected = function(infoName) {
+		return this.currentInfo === infoName;
+	}
+
+})
