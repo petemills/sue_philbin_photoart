a24f99a3f49ac4b70ab91ac9ccf11954addb03f0
Pete <pete@pete.com>
2014-09-07 20:59:10 +1200

    Minor changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('sue-philbin-photoart', []);
 
 app.controller("InfoController", function(){
-	this.currentInfo = 1;
+	this.currentInfo = 'aboutSue';
 
 	this.selectInfo = function(setInfo) {
 		this.currentInfo = setInfo;
